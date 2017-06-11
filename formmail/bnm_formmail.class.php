7b3fd9f295396543b8c9b499795d7979eb8d3827
tisoler <diegompaz@gmail.com>
2017-06-11 19:43:00 -0300

    bnm_formmail.class.php

--- formmail/bnm_formmail.class.php
+++ formmail/bnm_formmail.class.php
@@ -44,10 +44,10 @@ class bnm_formmail {
 
 		//$mail->Mailer   = "mail";
 		$mail->Host = "smtp.zoho.com"; //"127.0.0.1:25";
-		$mail->Port = 465;
+		$mail->Port = 587;
 		$mail->SMTPAuth = true;
 		$mail->SMTPDebug = 3;
-		$mail->SMTPSecure = 'ssl';
+		$mail->SMTPSecure = 'tls';
 		$mail->Mailer   = "mail";
 		$mail->Body    = $this->msg;
 		$mail->Subject = $this->subject;
